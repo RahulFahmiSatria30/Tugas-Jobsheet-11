32c71408a47ae78a8d21138d177ee2d778437885
rahul <rahul@LAPTOP-2OQLS66J>
2018-08-16 23:07:50 +0700

    Initial Commit

---
+++ src/jobsheet11/CobaOperasiString.java
@@ -0,0 +1,11 @@
+package jobsheet11;
+import java.util.Scanner;
+public class CobaOperasiString {
+    public static void main(String[] args) {
+        
+        String identitas = "Rahul Fahmi Satria / XRPL2 / 28";
+        System.out.println("Identitas : " + identitas);
+
+    }
+    
+}
