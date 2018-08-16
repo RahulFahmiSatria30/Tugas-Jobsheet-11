2d093202e2c292f0b1296c359a8a442868040fe6
rahul <rahul@LAPTOP-2OQLS66J>
2018-08-16 23:20:33 +0700

    initial Commit

---
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -0,0 +1,12 @@
+package jobsheet11;
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+
+    }
+  
+
+}
